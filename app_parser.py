3ebf7b61e36be75e3214b8884f5fa948a7e4f50c
Vladislav <109065019+TimVladislav13010@users.noreply.github.com>
2023-03-12 22:20:36 +0200

    commit script 4

--- app_parser.py
+++ app_parser.py
@@ -4,11 +4,12 @@ import argparse
 """
 Parser command in script.
 Commands for script.
+py main.py -a add_data -au /authors.json -qu /quotes.json
 """
 
 
 def arg_parser():
-    parser = argparse.ArgumentParser(description='A console utility.')
+    parser = argparse.ArgumentParser(description='Scripts for uploading json files to a cloud database')
 
     parser.add_argument('-a', '--add_data', type=str, default=None)
     parser.add_argument('-au', '--authors', type=str, default=None)
